1bef727c40d37c3667c5ee6dabab111212fb1ceb
niwanekochan <pooonda.0607@gmail.com>
2021-09-28 04:53:57 +0000

    bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    user_path(id: current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  before_action :authenticate_user!,except: [:top, :about]
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '103f3935ca032632cd81317fbc57e30f8a75a688d60afebc119e04506c41d2ebd467e00f6ed8ee18458796c6eb62e419115227ed08503c6b320fd10c82e98187'
