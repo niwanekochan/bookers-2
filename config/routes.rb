63c15232887c70247fc2fe4995b051a31673462b
niwanekochan <pooonda.0607@gmail.com>
2021-10-06 06:05:01 +0000

    ouyou-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only: [:edit, :update, :index, :show]
+  resources :users, only: [:edit, :update, :index, :show] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books, only: [:show, :create, :index, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+
 end
