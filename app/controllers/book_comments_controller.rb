8fee41aebb80d0ff3951d4a90b7cd5284cd2d18b
niwanekochan <pooonda.0607@gmail.com>
2021-10-12 10:11:20 +0000

    ouyou-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,21 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comments_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(params[:book_id])
+    @comment = current_user.book_comments.new(book_comments_params)
+    @comment.book_id = book.id
+    if @comment.save
     else
       @user = book.user
       @newbook = Book.new
       @book = Book.find(params[:book_id])
       @book_comments = BookComment.all
-      @book_comment = comment
-      render 'books/show'
+      @book_comment = @comment
     end
   end
 
   def destroy
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,20 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+  def self.search(search,word)
+    if search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "perfect_match"
+      @user = User.where("name LIKE?","#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   attachment :profile_image
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20}
